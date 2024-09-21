22b3a76fc8b16db439a033f19438e28b797a5044
Johan <jsbollll@gmail.com>
2024-09-21 20:13:28 +0200

    begun on pile creation

--- src/main.go
+++ src/main.go
@@ -8,17 +8,31 @@ import (
 	"strconv"
 	"encoding/json"
 	"strings"
+	"math/rand"
+)
+
+type CardType int
+const (
+	CARD_PEPPER CardType = iota
+	CARD_LETTUCE CardType = iota
+	CARD_CARROT CardType = iota
+	CARD_CABBAGE CardType = iota
+	CARD_ONION CardType = iota
+	CARD_TOMATO CardType = iota
+	VEGETABLE_TYPE_NUM = iota
 )
 
 const (
-	CARD_PEPPER = iota
-	CARD_LETTUCE = iota
-	CARD_CARROT = iota
-	CARD_CABBAGE = iota
-	CARD_ONION = iota
-	CARD_TOMATO = iota
+	PLAY_PILES_NUM = 3
 )
 
+
+func assert(c bool) {
+	if !c {
+		s := fmt.Sprintf("assertion failed %v", c)
+		panic(s)
+	}
+}
 
 type JCriteria struct {
 	PEPPER string
@@ -32,12 +46,27 @@ type JCriteria struct {
 type JCard struct {
 	Id int
 	Criteria JCriteria
-
 } 
 
 type JCards struct {
 	Cards []JCard
 }
+
+type Card struct {
+	Id int
+	Vegetable_type CardType
+	Vegetable_side bool
+}
+// actors are players and bots
+type GameState struct {
+	piles [][]Card
+	market []Card
+	actor_piles [][]Card
+	active_actor int
+	player_num int
+	bot_num int
+}
+
 
 func getNumPlayerBotConfigInput(reader *bufio.Reader, prompt string, min int, max int) (int, int, error) {
 	var player_num int
@@ -74,6 +103,58 @@ func getNumPlayerBotConfigInput(reader *bufio.Reader, prompt string, min int, ma
 	return player_num, bot_num, err
 }
 
+func createGameState(json_cards *JCards, player_num int, bot_num int) GameState {
+	actor_num := player_num + bot_num
+	
+	var ids []int
+	for _, jcard := range json_cards.Cards {
+		ids = append(ids, jcard.Id)
+	}
+
+	per_vegetable_num := actor_num * 3
+	var deck []Card
+	
+	for i := 0; i < VEGETABLE_TYPE_NUM; i += 1 {
+		rand.Shuffle(len(ids), func(i int, j int) {
+			ids[i], ids[j] = ids[j], ids[i]
+		})
+
+		for j := 0; j < per_vegetable_num; j += 1 {
+			card := Card{
+				Id: ids[j], 
+				Vegetable_type: CardType(i), 
+				Vegetable_side: false,
+			}
+			deck = append(deck, card)
+
+		}
+	}
+	rand.Shuffle(len(deck), func(i int, j int) {
+		deck[i], deck[j] = deck[j], deck[i]
+	})
+
+
+	s := GameState{}
+	pile_size := len(deck) / PLAY_PILES_NUM
+	pile_size_remainder := len(deck) % PLAY_PILES_NUM
+	assert(pile_size_remainder == 0)
+
+	index := 0
+	for i := 0; i < PLAY_PILES_NUM; i += 1 {
+		s.piles = append(s.piles, []Card{})
+		for j := 0; j < pile_size; j += 1 {
+			s.piles[i] = append(s.piles[i], deck[index])
+			index += 1
+		}
+	}
+
+	s.active_actor = rand.Intn(actor_num)
+	s.player_num = player_num
+	s.bot_num = bot_num
+
+	return s
+}
+
 func main() {
 	data, err := os.ReadFile("PointSaladManifest.json")
 	if err != nil {
@@ -89,13 +170,14 @@ func main() {
 	}
 	reader := bufio.NewReader(os.Stdin)
 
-	player_num, bot_num, err := getNumPlayerBotConfigInput(reader, "type number of players,bots example 0,1", 2, 6)
+	player_num, bot_num, err := getNumPlayerBotConfigInput(reader, "type number of players,bots example 1,1", 2, 6)
 	if err != nil {
 		log.Fatal(err)
 	}
-	actor_num := player_num + bot_num
 	
-	
+	s := createGameState(&json_cards, player_num, bot_num)	
+
+	fmt.Printf("%+v\n", s)
 
 	for true {
 		str, err := reader.ReadString('\n')
